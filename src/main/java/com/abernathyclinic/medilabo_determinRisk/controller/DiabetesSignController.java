8673396aca9d73d064c70df8305ee905cefef242
https://github.com/somykm <sk43@hood.edu>
2025-11-21 19:48:50 -0500

    Inorder to tells Spring Boot to allow requests coming from http://localhost:8082 to access this controller’s endpoints, added @CrossOrigin. And inorder to mark the class as a Spring MVC controller where every method returns a response body, added @RestController

--- src/main/java/com/abernathyclinic/medilabo_determinRisk/controller/DiabetesSignController.java
+++ src/main/java/com/abernathyclinic/medilabo_determinRisk/controller/DiabetesSignController.java
@@ -6,11 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
 @Slf4j
-@Controller
+@CrossOrigin(origins = "http://localhost:8082")
+@RestController
 @RequestMapping("/api/diabetes")
 public class DiabetesSignController {
     private final DiabetesReportService reportingService;
